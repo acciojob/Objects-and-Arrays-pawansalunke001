3691a9b1165bccae76c34e3b6a9f74b59d6210c4
Pawan Salunke <133101693+pawansalunke001@users.noreply.github.com>
2023-08-23 16:02:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ const person = {
 // Create a reference to the players array
 const team = players;
 
-// Create a copy of the players array
-const team1 = players.slice();
+// Create a copy of the players array using the spread operator
+const team1 = [...players];
 
-// Create a copy of the person object
-const cap1 = Object.assign({}, person);
+// Create a copy of the person object using object destructuring
+const cap1 = { ...person };
 
 // Testing: Modify players array and person object to see if copies are affected
 players[0] = "Mike";
@@ -23,10 +23,3 @@ console.log("Reference to players array (team):", team);
 console.log("Copy of players array (team1):", team1);
 console.log("Original person object:", person);
 console.log("Copy of person object (cap1):", cap1);
-// Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
-
-// window.players = players;
-// window.person = person;
-// window.team = team;
-// window.team1 = team1;
-// window.cap1 = cap1;
